a6629529b9e48022fb1dd1454d968b3e845815b4
Jesús Gallego <jesusgallego@outlook.com>
2021-10-08 12:58:36 +0200

    feat(core): :sparkles: useHandleApplePayment utility hook

--- packages/core/src/Payments/Apple/index.ts
+++ packages/core/src/Payments/Apple/index.ts
@@ -1,5 +1,8 @@
-import { CartFragment } from "@yownes/api";
+import { CartFragment, useCreatePaymentIntent } from "@yownes/api";
 import type { ApplePay } from "@stripe/stripe-react-native";
+import { useApplePay } from "@stripe/stripe-react-native";
+
+export { ApplePayButton } from "@stripe/stripe-react-native";
 
 export function cartToApplePay(cart: CartFragment): ApplePay.PresentParams {
   return {
@@ -11,4 +14,35 @@ export function cartToApplePay(cart: CartFragment): ApplePay.PresentParams {
   };
 }
 
-export { ApplePayButton, useApplePay } from "@stripe/stripe-react-native";
+export function useHandleApplePayment(
+  cart: CartFragment,
+  onSuccess?: () => void
+) {
+  const { presentApplePay, confirmApplePayPayment, isApplePaySupported } =
+    useApplePay();
+  const [createPaymentIntent] = useCreatePaymentIntent();
+  async function handlePayment() {
+    if (cart) {
+      const { error, paymentMethod } = await presentApplePay(
+        cartToApplePay(cart)
+      );
+      const { data: intentData } = await createPaymentIntent({
+        variables: { paymentMethod: paymentMethod?.id },
+      });
+      if (intentData?.createPaymentIntent?.clientSecret) {
+        const { error: appleError } = await confirmApplePayPayment(
+          intentData.createPaymentIntent.clientSecret
+        );
+        console.log({ appleError });
+        if (appleError) {
+        } else {
+          onSuccess?.();
+        }
+      }
+    }
+  }
+  return {
+    isApplePaySupported,
+    handlePayment,
+  };
+}
