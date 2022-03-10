c45db6945dc5698c1b5a053117a2ad81e95440ae
agarciaguillo <albertogarciaelx@gmail.com>
2022-03-10 12:40:17 +0100

    feat: billing info removed from payment method

--- apps/dashboard/src/api/types/UpdateCustomer.ts
+++ apps/dashboard/src/api/types/UpdateCustomer.ts
@@ -9,10 +9,35 @@ import { CustomerInput } from "./globalTypes";
 // GraphQL mutation operation: UpdateCustomer
 // ====================================================
 
+export interface UpdateCustomer_updateCustomer_customer {
+  __typename: "StripeCustomerType";
+  /**
+   * The ID of the object.
+   */
+  id: string;
+  /**
+   * The customer's address.
+   */
+  address: any | null;
+  /**
+   * The customer's full name or business name.
+   */
+  name: string;
+  /**
+   * The customer's phone number.
+   */
+  phone: string;
+  /**
+   * A set of key/value pairs that you can attach to an object. It can be useful for storing additional information about an object in a structured format.
+   */
+  metadata: any | null;
+}
+
 export interface UpdateCustomer_updateCustomer {
   __typename: "UpdateCustomerMutation";
   ok: boolean | null;
   error: string | null;
+  customer: UpdateCustomer_updateCustomer_customer | null;
 }
 
 export interface UpdateCustomer {
