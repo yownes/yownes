25b6047dc5e15fc7b63c04433e4af5a641fd6bee
agarciaguillo <albertogarciaelx@gmail.com>
2022-07-08 16:44:07 +0200

    feat(default-theme): :sparkles: ErrorPayments and NoOrders placeholders

--- apps/default-theme/src/components/images/index.ts
+++ apps/default-theme/src/components/images/index.ts
@@ -1,7 +1,9 @@
 export { default as Billing } from "./Billing";
 export { default as EmptyCart } from "./EmptyCart";
+export { default as ErrorPayment } from "./ErrorPayment";
 export { default as Login } from "./Login";
 export { default as NoFavs } from "./NoFavs";
+export { default as NoOrders } from "./NoOrders";
 export { default as Register } from "./Register";
 export { default as Shipping } from "./Shipping";
 export { default as Success } from "./Success";
