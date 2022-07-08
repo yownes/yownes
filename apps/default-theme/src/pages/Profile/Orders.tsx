6b5680a8e749fba79316614c679a02b49bf802ea
agarciaguillo <albertogarciaelx@gmail.com>
2022-07-08 16:49:18 +0200

    feat(default-theme): :zap: No orders placeholder

--- apps/default-theme/src/pages/Profile/Orders.tsx
+++ apps/default-theme/src/pages/Profile/Orders.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import { FlatList, Pressable } from "react-native";
 import { useGetOrders } from "@yownes/api";
 
-import { Box } from "../../components/atoms";
+import { Box, Text } from "../../components/atoms";
 import { Order } from "../../components/molecules";
+import NoOrdersImage from "../../components/images/NoOrders";
 import type { OrdersProps } from "../../navigation/Profile";
 import { useTheme } from "../../lib/theme";
 import filterNulls from "../../lib/filterNulls";
+
+const OrdersPlaceholder = () => (
+  <Box padding="l">
+    <NoOrdersImage />
+    <Text variant="header2" textAlign="center" marginVertical="l">
+      No has realizado ningún pedido
+    </Text>
+  </Box>
+);
 
 const Orders = ({ navigation }: OrdersProps) => {
   const { data } = useGetOrders();
@@ -20,6 +30,7 @@ const Orders = ({ navigation }: OrdersProps) => {
         paddingTop: theme.spacing.m,
         paddingHorizontal: theme.spacing.m,
       }}
+      ListEmptyComponent={<OrdersPlaceholder />}
       renderItem={({ item }) => (
         <Pressable
           onPress={() => {
