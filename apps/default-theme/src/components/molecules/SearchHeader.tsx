9d436a2f83ed4a65b55331ec9cb9bfc12fc1478f
agarciaguillo <albertogarciaelx@gmail.com>
2022-07-13 11:11:22 +0200

    refactor(default-theme): :lipstick: Search header style

--- apps/default-theme/src/components/molecules/SearchHeader.tsx
+++ apps/default-theme/src/components/molecules/SearchHeader.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useTheme } from "@shopify/restyle";
+
+import type { Theme } from "../../lib/theme";
 
 import { Box, Card, Input } from "../atoms";
 import { Cross, Search } from "../icons";
@@ -11,29 +14,39 @@ interface SearchHeaderProps {
 }
 
 const SearchHeader = ({ onChange, value }: SearchHeaderProps) => {
+  const theme = useTheme<Theme>();
   return (
-    <Card padding="m" justifyContent="center">
+    <Card paddingHorizontal="l" paddingVertical="m" justifyContent="center">
       <SafeAreaView edges={["top"]}>
         <Box alignItems="center" flexDirection="row">
-          <Box position="absolute">
-            <Search color="greyscale4" size={15} />
+          <Box position="absolute" left={15} zIndex={9}>
+            <Search color="greyscale3" size={15} />
           </Box>
           <Box flex={1}>
             <Input
               placeholder="Buscar..."
+              placeholderTextColor={theme.colors.greyscale3}
               onChangeText={onChange}
               value={value}
-              style={{ paddingLeft: 25 }}
+              style={{
+                backgroundColor: theme.colors.greyscale2,
+                borderRadius: 40,
+                borderBottomWidth: 0,
+                color: theme.colors.greyscale3,
+                paddingHorizontal: 40,
+              }}
             />
           </Box>
           {value !== "" && (
-            <TouchableOpacity
-              onPress={() => {
-                onChange("");
-              }}
-            >
-              <Cross color="greyscale4" size={15} />
-            </TouchableOpacity>
+            <Box position="absolute" right={15}>
+              <TouchableOpacity
+                onPress={() => {
+                  onChange("");
+                }}
+              >
+                <Cross color="greyscale3" size={15} />
+              </TouchableOpacity>
+            </Box>
           )}
         </Box>
       </SafeAreaView>
